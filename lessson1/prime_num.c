f407f20fbd4c72694c5d13b99626b84307cba56a
quepum <118292850+quepum@users.noreply.github.com>
2024-09-10 12:56:13 +0300

    Create prime_num.c

---
+++ lessson1/prime_num.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int n, cnt;
+    scanf("%d", &n);
+    for(int i=1; i <= n; i+=1)
+    {
+        cnt = 0;
+        for(int j = 1; j <= i; j += 1)
+        {
+            if (i % j == 0) {cnt += 1;}
+        }
+        if(cnt == 2 || i == 1) {printf("%d\t", i);}
+    }
+    
+    return 0;
+}
