105f6da65f22c7e5763754c3d720f7e96997b70b
quepum <118292850+quepum@users.noreply.github.com>
2024-09-23 22:53:10 +0300

    Update chastnoe.c

--- lessson1/chastnoe.c
+++ lessson1/chastnoe.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 #include <math.h>
 
-int func(int num1, int num2) {
-    int result = 0, divisible = fabs(num1), divisor = fabs(num2);
-    while (divisible >= divisor) {
-        divisible -= divisor;
+int incompleteQuotientCounter(const int num1, const int num2) {
+    int result = 0;
+    int dividend = abs(num1);
+    int divisor = abs(num2);
+
+    while (dividend >= divisor) {
+        dividend -= divisor;
         ++result;
     }
-    if ((num1 >= 0 && num2 > 0) || (num1 <= 0 && num2 < 0)) {
+
+    if (dividend == 0) {
+        if (num1 < 0 || num2 < 0) {
+            return -result;
+        }
         return result;
-    } else {
-        return -result;
     }
+    if (num1 >= 0 && num2 > 0) {
+        return result;
+    }
+    if (num1 <= 0 && num2 < 0) {
+        return (result + 1);
+    }
+    return -(result + 1);
 }
 
 int main(void) {
     int a = 0, b = 0;
+
     printf("Введите делимое и делитель\n");
     scanf("%d %d", &a, &b);
+
     if (b != 0) {
-        printf("Неполное частное равно %d", func(a, b));
-    } else {
+        printf("Неполное частное равно %d", incompleteQuotientCounter(a, b));
+    }
+    else {
         printf("%s", "error");
     }
+
     return 0;
 }
