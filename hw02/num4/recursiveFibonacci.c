229eb1f6400e6d9ab2c99c9bbc4800a4e51128c7
quepum <118292850+quepum@users.noreply.github.com>
2024-09-25 11:52:04 +0300

    Update recursiveFibonacci.c

--- hw02/num4/recursiveFibonacci.c
+++ hw02/num4/recursiveFibonacci.c
@@ -1,1 +1,36 @@
+#include <stdbool.h>
+#include <stdio.h>
 
+long long int recursiveMethod(int num) {
+    if (num == 1 || num == 2) {
+        return 1;
+    }
+    return recursiveMethod(num - 1) + recursiveMethod(num - 2);
+}
+
+bool test() {
+    int currentSumm = 0;
+    int correctAnswer = 17710;
+    for (int i = 1; i <= 20; ++i) {
+        currentSumm += recursiveMethod(i);
+    }
+    return correctAnswer == currentSumm;
+}
+
+int main(void) {
+    if (!test()) {
+        printf("%s", "Test failed");
+        return 1;
+    }
+
+    int numberOfFibonacci = 0;
+
+    printf("%s", "Enter how many fibonacci numbers you want to get:");
+    scanf("%d", &numberOfFibonacci);
+    printf("%s", "Answer:\n");
+
+    for (int i = 1; i <= numberOfFibonacci; ++i) {
+        printf("%d.%lld\n",i, recursiveMethod(i));
+    }
+    return 0;
+}
