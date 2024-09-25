c4c60329d5264a500b1710e3890554d2604e539f
quepum <118292850+quepum@users.noreply.github.com>
2024-09-25 11:45:05 +0300

    Update fibonacciIterative.c

--- hw02/num4/fibonacciIterative.c
+++ hw02/num4/fibonacciIterative.c
@@ -1,1 +1,45 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+long long int iterativeMethod(int num) {
+    long long int element1 = 0, element2 = 1;
+    if (num == 1 || num == 2) {
+        return 1;
+    }
+    else {
+        for (int i = 2; i <= num; ++i) {
+            long long int currentElement = element1 + element2;
+            element1 = element2;
+            element2 = currentElement;
+        }
+    }
+    return element2;
+}
+
+bool test() {
+    int currentSumm = 0;
+    int correctAnswer = 17710;
+    for (int i = 1; i <= 20; ++i) {
+        currentSumm += iterativeMethod(i);
+    }
+    return correctAnswer == currentSumm;
+}
+
+int main(void) {
+    if (!test()) {
+        printf("%s", "Test failed");
+        return 1;
+    }
+
+    int numberOfFibonacci = 0;
+
+    printf("%s", "Enter how many fibonacci numbers you want to get\n");
+    scanf("%d", &numberOfFibonacci);
+    printf("%s", "Answer:\n");
+
+    for (int i = 1; i <= numberOfFibonacci; ++i) {
+        printf("%d.%lld\n",i, iterativeMethod(i));
+    }
+    return 0;
+}
 
