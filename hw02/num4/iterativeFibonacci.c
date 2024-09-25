2df633781cc0cd888dc955834a9d6da5e8bc7d1d
quepum <118292850+quepum@users.noreply.github.com>
2024-09-25 21:29:58 +0300

    /

--- hw02/num4/iterativeFibonacci.c
+++ hw02/num4/iterativeFibonacci.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 long long int iterativeMethod(int num) {
-    long long int element1 = 0, element2 = 1;
+    long long int element1 = 0;
+    long long element2 = 1;
     if (num == 1 || num == 2) {
         return 1;
-    }
-    else {
+    } else {
         for (int i = 2; i <= num; ++i) {
             long long int currentElement = element1 + element2;
             element1 = element2;
@@ -17,24 +17,34 @@ long long int iterativeMethod(int num) {
 }
 
 bool test() {
-    int currentSumm = 0;
-    int correctAnswer = 17710;
-    for (int i = 1; i <= 20; ++i) {
-        currentSumm += iterativeMethod(i);
+    int collectionFibonacci[10] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+    for (int i = 1; i <= 10; ++i) {
+        if (iterativeMethod(i) != collectionFibonacci[i - 1]) {
+            return false;
+        }
     }
-    return correctAnswer == currentSumm;
+    return true;
 }
 
 int main(void) {
     if (!test()) {
         printf("%s", "Test failed");
         return 1;
+    } else {
+        printf("%s", "The tests were passed successfully\n");
     }
 
     int numberOfFibonacci = 0;
 
-    printf("%s", "Enter how many fibonacci numbers you want to get\n");
+    printf("%s", "Enter how many fibonacci numbers you want to get(no more than 60):");
     scanf("%d", &numberOfFibonacci);
+
+    while (numberOfFibonacci <= 0 || numberOfFibonacci > 60) {
+        printf("Enter a natural number no more than 60!\n");
+        printf("%s", "Enter how many fibonacci numbers you want to get:");
+        scanf("%d", &numberOfFibonacci);
+    }
+
     printf("%s", "Answer:\n");
 
     for (int i = 1; i <= numberOfFibonacci; ++i) {
@@ -42,4 +52,3 @@ int main(void) {
     }
     return 0;
 }
-
