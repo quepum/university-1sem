1cd2314e5c803ce42a5d4d125821db1b951a6b0a
quepum <118292850+quepum@users.noreply.github.com>
2024-10-09 18:56:46 +0300

    /

--- hw03/num3.c
+++ hw03/num3.c
@@ -1,6 +1,9 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define TEST_SIZE 100
 
 void swap(int *firstValue, int *secondValue) {
     int copy = *firstValue;
@@ -38,6 +41,61 @@ void quickSort(int inputCollection[], const int low, int high) {
     quickSort(inputCollection, i + 1, high);
 }
 
+bool checkSorting(const int testingArray[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (testingArray[i] > testingArray[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool test1(){
+    int *collectionForTesting = malloc(TEST_SIZE * sizeof(int));
+    if (collectionForTesting == NULL) {
+        printf("ERROR: memory is not available!\n");
+        return false;
+    }
+    for (int i = 0; i < TEST_SIZE; i++) {
+        collectionForTesting[i] = rand() % 100;
+    }
+    quickSort(collectionForTesting, 0, TEST_SIZE - 1);
+    if (!checkSorting(collectionForTesting, TEST_SIZE)) {
+        free(collectionForTesting);
+        printf("test1");
+        return false;
+    }
+    free(collectionForTesting);
+    return true;
+}
+
+bool test2() {
+    int collectionForTesting[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    quickSort(collectionForTesting, 0, 9);
+    if (!checkSorting(collectionForTesting, 10)) {
+        return false;
+    }
+    return true;
+}
+
+bool test3() {
+    int collectionForTesting[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    quickSort(collectionForTesting, 0, 9);
+    return checkSorting(collectionForTesting, 10);
+}
+
+bool test4() {
+    int collectionForTesting[10] = {1, 3, 7, 2, 4, 6, 9, 1, 1, 100};
+    quickSort(collectionForTesting, 0, 9);
+    return checkSorting(collectionForTesting, 10);
+}
+
+bool test5() {
+    int collectionForTesting[10] = {100, 3, 7, 2, 4, 6, 9, 1, 1, 0};
+    quickSort(collectionForTesting, 0, 9);
+    return checkSorting(collectionForTesting, 10);
+}
+
 void printer(int array[], const int size) {
     for (int i = 0; i < size; ++i) {
         printf("%d\t", array[i]);
@@ -46,6 +104,13 @@ void printer(int array[], const int size) {
 }
 
 int main() {
+    if (!(test1() && test2() && test3() && test4() && test5())) {
+        printf("Tests failed, something went wrong");
+        return 1;
+    } else {
+        printf("%s", "The tests were passed successfully\n");
+    }
+
     int size = 0;
     printf("Enter the size of the array:\n");
     scanf("%d", &size);
