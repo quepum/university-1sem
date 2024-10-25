5ca1b453e9237ea855cb015539f98c67f2492c85
quepum <118292850+quepum@users.noreply.github.com>
2024-10-25 22:01:41 +0300

    implement the output of the required phone number by the entered name and vice versa

--- hw04/num3/num3.c
+++ hw04/num3/num3.c
@@ -3,7 +3,6 @@
 
 #define SIZE 100
 
-
 typedef struct Entry {
     char name[SIZE];
     char number[SIZE];
@@ -28,6 +27,7 @@ int readEntries(const char* fileName, Entry collection[]){
 // функция добавить запись
 void addEntry(){
 }
+
 //распечатать все записи
 void displayContent(int numLines, Entry collection[]){
     printf("Содержание телефонного справочника\n");
@@ -37,17 +37,24 @@ void displayContent(int numLines, Entry collection[]){
     printf("Конец\n");
 }
 //найти телефон по имени
-void searchTelephoneByName(const char name[], int numLines, Entry collection[]){
+int searchTelephoneByName(const char name[], const int numLines, const Entry collection[]){
     for (int i = 0; i < numLines; ++i){
         if (strcmp(collection[i].name, name) == 0){
-            printf("%s\n", collection[i].number);
+            return i;
         }
     }
-    printf("Такого имени нет в справочнике\n");
+    return -1;
 
 }
+
 //найти имя по телефону
-void searchNameByTelephone(){
+int searchNameByTelephone(const char phoneNumber[], const int numLines, const Entry collection[]){
+    for (int i = 0; i < numLines; ++i){
+        if (strcmp(collection[i].number, phoneNumber) == 0){
+            return i;
+        }
+    }
+    return -1;
 
 }
 //сохранить файл
@@ -55,7 +62,7 @@ void save(){
 
 }
 
-void printCommands(){
+void printCommands(void){
     printf("ТЕЛЕФОННЫЙ СПРАВОЧНИК\n");
     printf("0 - выйти\n"
            "1 - добавить запись (имя и телефон)\n"
@@ -82,13 +89,33 @@ int main(void){
                 displayContent(numberOfLines, collection);
                 break;
             case 3:
-                break;
+                printf("Введите имя контакта:\n");
+                char name[SIZE] = {'\0'};
+                scanf("%s", name);
+                int resultOfSearchingByName = searchTelephoneByName(name, numberOfLines, collection);
+                if (resultOfSearchingByName != -1){
+                    printf("%s\n", collection[resultOfSearchingByName].number);
+                    break;
+                } else{
+                    printf("Такого имени нет в телефонном справочнике\n");
+                    break;
+                }
             case 4:
+                printf("Введите номер телефона контакта:\n");
+                char phoneNumber[SIZE] = {'\0'};
+                scanf("%s", phoneNumber);
+                int resultOfSearchingByPhone = searchNameByTelephone(phoneNumber, numberOfLines, collection);
+                if (resultOfSearchingByPhone != -1){
+                    printf("%s\n", collection[resultOfSearchingByPhone].name);
+                } else{
+                    printf("Такого номера нет в телефонном справочнике\n");
+                }
                 break;
             case 5:
                 break;
             default:
                 printf("Такой команды не существует, введите номер снова\n");
+                break;
         }
     } while (command !=0);
 
