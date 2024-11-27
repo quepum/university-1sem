91063b8ff10b1083bffbbe6855a42d0e7c9f2b7c
quepum <118292850+quepum@users.noreply.github.com>
2024-11-27 19:27:32 +0300

    add node structure, funcs for making and removing nodes

---
+++ hw08/tree.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+typedef struct Node {
+    const char *key;
+    const char *value;
+    struct Node *leftChild;
+    struct Node *rightChild;
+    int height;
+} Node;
+
+Node *createNewNode(const char *key, const char *value) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->key = strdup(key);
+    node->value = strdup(value);
+    node->leftChild = NULL;
+    node->rightChild = NULL;
+    node->height = 1;
+    return node;
+}
+
+void freeNode(Node *node) {
+    if (node) {
+        free((void *)node->key);
+        free((void *)node->value);
+        free(node);
+    }
+}
